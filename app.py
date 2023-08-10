1dae26d1f61c3e216f38d9dd397ac61cb70e8dc1
yma415 <yma415@gatech.edu>
2023-08-10 16:32:37 -0400

    enable chinese characters

--- app.py
+++ app.py
@@ -2,16 +2,17 @@ import sqlite3
 from kivymd.app import MDApp
 from kivymd.uix.list import OneLineListItem
 from kivy.uix.screenmanager import ScreenManager, Screen
-from dishes import Dish
+from recipes import Recipe
 from kivymd.theming import ThemeManager
+
 
 class TitleScreen(Screen):
     pass
 
-class NewDishScreen(Screen):
+class NewRecipeScreen(Screen):
     pass
 
-class DishListScreen(Screen):
+class RecipeListScreen(Screen):
     pass
 
 class MealPlannerApp(MDApp):
@@ -21,9 +22,9 @@ class MealPlannerApp(MDApp):
 
         self.conn = sqlite3.connect('mealplanner.db')
         self.cursor = self.conn.cursor()
-        self.cursor.execute('''CREATE TABLE IF NOT EXISTS dishes (name TEXT, meal_type TEXT, ingredients TEXT, extra_instructions TEXT)''')
+        self.cursor.execute('''CREATE TABLE IF NOT EXISTS recipes (name TEXT, meal_type TEXT, ingredients TEXT, extra_instructions TEXT)''')
         self.conn.commit()
-        self.dishes = []
+        self.recipes = []
 
     def build(self):
         self.theme_cls.primary_palette = 'Red' #  'Cyan', 'Teal'
@@ -35,32 +36,39 @@ class MealPlannerApp(MDApp):
         self.title_screen = TitleScreen(name="title")
         self.screen_manager.add_widget(self.title_screen)
 
-        self.new_dish_screen = NewDishScreen(name="new_dish")
-        self.screen_manager.add_widget(self.new_dish_screen)
+        self.new_recipe_screen = NewRecipeScreen(name="new_recipe")
+        self.screen_manager.add_widget(self.new_recipe_screen)
 
-        self.dish_list_screen = DishListScreen(name="dish_list")  
-        self.screen_manager.add_widget(self.dish_list_screen) 
+        self.recipe_list_screen = RecipeListScreen(name="recipe_list")  
+        self.screen_manager.add_widget(self.recipe_list_screen) 
 
         return self.screen_manager
 
-    def switch_to_dish_list_screen(self):
-        self.root.current = 'dish_list'  # Transition to the dish list screen
-        self.load_dishes_in_list_screen()  # Load and display the list of dishes
+    def switch_to_recipes_list_screen(self):
+        self.root.current = "recipe_list"
+        self.load_recipes_in_list_screen()  
 
-    def load_dishes_in_list_screen(self):
-        self.dish_list_screen.ids.dish_grid.clear_widgets()
-        self.cursor.execute("SELECT * FROM dishes")
-        loaded_dishes = self.cursor.fetchall()
-        self.dishes = [Dish(*row) for row in loaded_dishes]
-        for dish in self.dishes:
+    def switch_to_new_recipe_screen(self):
+        self.root.current = "new_recipe"
+        self.load_new_recipe_screen()  
+
+    def load_new_recipe_screen(self):
+        pass  
+
+    def load_recipes_in_list_screen(self):
+        self.recipe_list_screen.ids.recipe_grid.clear_widgets()
+        self.cursor.execute("SELECT * FROM recipes")
+        self.recipes = [Recipe(*row) for row in self.cursor.fetchall()]
+        print(self.recipes)
+        for dish in self.recipes:
             dish_label = OneLineListItem(text=dish._name)
-            dish_label.bind(on_release=lambda instance: self.modify_dish(instance.text))
-            self.dish_list_screen.ids.dish_grid.add_widget(dish_label)
+            dish_label.bind(on_release=lambda instance: self.modify_recipe(instance.text))
+            self.recipe_list_screen.ids.recipe_grid.add_widget(dish_label)
 
-    def modify_dish(self, dish_name):
-        # Find the Dish object based on dish_name
-        selected_dish = next((dish for dish in self.dishes if dish._name == dish_name), None)
-        if selected_dish:
+    def modify_recipe(self, selected_name):
+        # Find the Recipe object based on recipe name
+        selected_recipe = next((r for r in self.recipes if recipe._name == selected_name), None)
+        if selected_recipe:
             # Transition to a new screen to modify the selected_dish
             # You need to create a new screen class for dish modification
             # and implement the necessary UI and logic for it.
--- recipes.py
+++ recipes.py
@@ -1,6 +1,6 @@
 
 # may add a class for gredient, so some gredient can alter users to prepare earlier 
-class Dish:
+class Recipe:
     def __init__(self, name, meal_type, ingredients, extra_instructions= ''):
         self._name = name
         self._meal_type = meal_type 
@@ -8,4 +8,4 @@ class Dish:
         self._extra_instructions = extra_instructions
 
     def __repr__(self) -> str:
-        return f'Dish(name = {self._name}, meal_type ={self._meal_type}, ingredients = {self._ingredients}, extra_instruction = {self._extra_instructions})'
+        return f'Recipe(name = {self._name}, meal_type ={self._meal_type}, ingredients = {self._ingredients}, extra_instruction = {self._extra_instructions})'
